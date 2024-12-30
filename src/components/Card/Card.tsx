c51f18213ec280ddcb864d80d36726c2cbdbb540
pml-emerson <efranca@paradisemobile.com>
2024-12-30 00:13:45 -0300

    Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,20 @@
 import { Character } from "@/pages/Home/types";
 import { FC } from "react";
+import { ReactComponent as FavoriteIcon } from "./assets/favorito_02.svg";
+import "./Card.css";
 
-export const Card: FC<Character> = ({ description, id, name, thumbnail }) => {
+export const Card: FC<Character> = ({ id, name, thumbnail }) => {
   return (
-    <div key={id}>
-      <h2>{name}</h2>
-      <p>{description}</p>
-      <img src={`${thumbnail.path}.${thumbnail.extension}`} alt={name} />
+    <div className="card" key={id}>
+      <img
+        className="card__image"
+        src={`${thumbnail.path}.${thumbnail.extension}`}
+        alt={name}
+      />
+      <div className="card__content">
+        <h1 className="card__title">{name}</h1>
+        <FavoriteIcon />
+      </div>
     </div>
   );
 };
