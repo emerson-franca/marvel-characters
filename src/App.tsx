818519984977ba5edebe15b97e68ed7be5860258
pml-emerson <efranca@paradisemobile.com>
2024-12-30 12:19:21 -0300

    react query provider config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Home } from "./pages/Home/Home";
 import "./styles/variables.css";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+      gcTime: 60 * 60 * 1000,
+    },
+  },
+});
+
 function App() {
   return (
-    <div className="App">
+    <QueryClientProvider client={queryClient}>
       <Home />
-    </div>
+    </QueryClientProvider>
   );
 }
 
