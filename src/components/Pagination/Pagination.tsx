ef01b2a60d464683c21c3a4d83ad2ee723ff92b1
pml-emerson <efranca@paradisemobile.com>
2024-12-31 14:48:15 -0300

    Pagination component

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,62 @@
+import "./Pagination.css";
+
+type PaginationProps = {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+};
+
+export const Pagination = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+}: PaginationProps) => {
+  const pagesToShow = 5;
+  const startPage =
+    Math.floor((currentPage - 1) / pagesToShow) * pagesToShow + 1;
+  const endPage = Math.min(startPage + pagesToShow - 1, totalPages);
+
+  const pages = Array.from(
+    { length: endPage - startPage + 1 },
+    (_, i) => startPage + i
+  );
+
+  return (
+    <div className="pagination">
+      <button onClick={() => onPageChange(1)} disabled={currentPage === 1}>
+        Primeira
+      </button>
+
+      <button
+        onClick={() => onPageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        Anterior
+      </button>
+
+      {pages.map((page) => (
+        <button
+          key={page}
+          onClick={() => onPageChange(page)}
+          className={currentPage === page ? "active" : ""}
+        >
+          {page}
+        </button>
+      ))}
+
+      <button
+        onClick={() => onPageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}
+      >
+        Próxima
+      </button>
+
+      <button
+        onClick={() => onPageChange(totalPages)}
+        disabled={currentPage === totalPages}
+      >
+        Última
+      </button>
+    </div>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./Card/Card";
 export * from "./SearchBar/SearchBar";
+export * from "./Pagination/Pagination";
