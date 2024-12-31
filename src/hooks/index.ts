508b8d5912563fec54e9e884b37eeb2c58618317
pml-emerson <efranca@paradisemobile.com>
2024-12-31 16:42:30 -0300

    home using favorites + pagination

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,2 @@
 export * from "./useMarvelCharacters";
+export * from "./useFavorites";
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,8 @@
-import { Card } from "../../components";
+import { Card, Pagination } from "../../components";
 import Logo from "../../assets/logo.svg";
 import "./Home.css";
 import SearchBar from "../../components/SearchBar/SearchBar";
-import { useMarvelCharacters } from "../../hooks/";
+import { useMarvelCharacters, useFavorites } from "../../hooks/";
 import { useState } from "react";
 import { ReactComponent as ToggleOn } from "./assets/toggle_on.svg";
 import { ReactComponent as ToggleOff } from "./assets/toggle_off.svg";
@@ -10,8 +10,17 @@ import { ReactComponent as FavoriteOn } from "../../assets/favorito_01.svg";
 import { ReactComponent as HeroIcon } from "../../assets/ic_heroi.svg";
 
 export const Home = () => {
+  const { isFavorite, favorites } = useFavorites();
   const [order, setOrder] = useState(false);
-  const { characters, handleSearch, isLoading } = useMarvelCharacters();
+
+  const {
+    characters,
+    handleSearch,
+    isLoading,
+    handlePageChange,
+    page,
+    totalPages,
+  } = useMarvelCharacters();
 
   return (
     <div className="home">
@@ -27,38 +36,49 @@ export const Home = () => {
       <SearchBar onSearch={handleSearch} />
 
       <section className="home__content">
-        {isLoading && (
+        {isLoading ? (
           <div className="home__loading__container">
             <span className="home__loading"></span>
           </div>
+        ) : (
+          <>
+            <div className="home__content__info">
+              <p>Encontrados {characters.length} heróis</p>
+
+              <div className="home__content__filters">
+                <div className="home__content__filters__wrapper">
+                  <HeroIcon />
+                  <span>Ordenar por nome - A/Z</span>
+                  {order ? (
+                    <ToggleOn onClick={() => setOrder(!order)} />
+                  ) : (
+                    <ToggleOff onClick={() => setOrder(!order)} />
+                  )}
+                </div>
+
+                <div className="home__content__filters__wrapper">
+                  <p>Somente favoritos</p>
+                  <FavoriteOn />
+                </div>
+              </div>
+            </div>
+
+            <div className="home__content__grid">
+              {characters.map((character) => (
+                <Card
+                  key={character.id}
+                  {...character}
+                  favorite={isFavorite(character.id)}
+                />
+              ))}
+            </div>
+            <Pagination
+              currentPage={page}
+              onPageChange={handlePageChange}
+              totalPages={totalPages}
+            />
+          </>
         )}
-
-        <div className="home__content__info">
-          <p>Encontrados {characters.length} heróis</p>
-
-          <div className="home__content__filters">
-            <div className="home__content__filters__wrapper">
-              <HeroIcon />
-              <span>Ordenar por nome - A/Z</span>
-              {order ? (
-                <ToggleOn onClick={() => setOrder(!order)} />
-              ) : (
-                <ToggleOff onClick={() => setOrder(!order)} />
-              )}
-            </div>
-
-            <div className="home__content__filters__wrapper">
-              <p>Somente favoritos</p>
-              <FavoriteOn />
-            </div>
-          </div>
-        </div>
-
-        <div className="home__content__grid">
-          {characters.map((character) => (
-            <Card key={character.id} {...character} />
-          ))}
-        </div>
       </section>
     </div>
   );
