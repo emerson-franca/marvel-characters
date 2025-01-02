b95dcbb780a84f8a6c00a622b9c15574d5337f95
pml-emerson <efranca@paradisemobile.com>
2025-01-02 02:30:27 -0300

    character pages using new api calls

--- src/pages/Character/Character.tsx
+++ src/pages/Character/Character.tsx
@@ -1,55 +1,123 @@
-import { useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import { fetchCharacterById } from "../../api/character";
+import { Link, useParams } from "react-router-dom";
 import Logo from "../../assets/logo_menor.svg";
-import SearchBar from "../../components/SearchBar/SearchBar";
 import "./Character.css";
+import {
+  useCharacterDetails,
+  useFavorites,
+  useCharacterComics,
+} from "../../hooks";
+import { ReactComponent as FavoriteOff } from "../../assets/favorito_02.svg";
+import { ReactComponent as FavoriteOn } from "../../assets/favorito_01.svg";
+import { ReactComponent as Comics } from "./assets/ic_quadrinhos.svg";
+import { ReactComponent as Trailer } from "./assets/ic_trailer.svg";
+import { Footer } from "../../components";
 
 export const Character = () => {
   const { id } = useParams<{ id: string }>();
-
-  const {
-    data: character,
-    isLoading,
-    isError,
-    error,
-  } = useQuery({
-    queryKey: ["character", id],
-    queryFn: async () => fetchCharacterById(id!),
-  });
+  const { isFavorite, toggleFavorite } = useFavorites();
+  const { character, isError, isLoading } = useCharacterDetails(id!);
+  const { comics, isLoading: isLoadingComics } = useCharacterComics(id!);
 
   if (isLoading) {
-    return <div>Carregando...</div>;
-  }
-
-  if (isError) {
     return (
-      <div>
-        Erro ao buscar personagem:{" "}
-        {error instanceof Error ? error.message : "Erro desconhecido"}
+      <div className="wrapper" role="status">
+        <span className="loading" />
       </div>
     );
   }
 
-  return (
-    <div>
-      <section className="character__header">
-        <img src={Logo} alt="marvel logo" />
-        <SearchBar onSearch={() => {}} />
+  if (isError) {
+    return (
+      <section className="wrapper" role="alert">
+        Erro ao buscar personagem
       </section>
+    );
+  }
 
-      {character ? (
-        <div>
-          <h1>{character.name}</h1>
-          <img
-            src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-            alt={character.name}
-          />
-          <p>{character.description || "Sem descrição disponível."}</p>
-        </div>
-      ) : (
-        <div>Personagem não encontrado</div>
-      )}
+  return (
+    <div className="character">
+      <header className="character__header">
+        <Link to="/">
+          <img src={Logo} alt="Marvel logo" />
+        </Link>
+      </header>
+
+      <main className="character__content">
+        {character && (
+          <>
+            <div className="character__content__wrapper">
+              <article className="character__details">
+                <header className="character__infos__header">
+                  <div className="flex">
+                    <h1>{character.name}</h1>
+                    <button
+                      className="character__infos__favorite"
+                      onClick={() => toggleFavorite(character)}
+                      aria-label={`${
+                        isFavorite(character.id) ? "Remover" : "Adicionar"
+                      } ${character.name} aos favoritos`}
+                    >
+                      {isFavorite(character.id) ? (
+                        <FavoriteOn />
+                      ) : (
+                        <FavoriteOff />
+                      )}
+                    </button>
+                  </div>
+                  <p>{character.description || "Sem descrição disponível."}</p>
+                </header>
+
+                <section className="character__stats">
+                  <div className="character__stat">
+                    <h3>Quadrinhos</h3>
+                    <div className="character__stat__content">
+                      <Comics />
+                      <p>{character.comics.available}</p>
+                    </div>
+                  </div>
+
+                  <div className="character__stat">
+                    <h3>Filmes</h3>
+                    <div className="character__stat__content">
+                      <Trailer />
+                      <p>{character.events.available}</p>
+                    </div>
+                  </div>
+                </section>
+              </article>
+              <figure className="character__image">
+                <img
+                  src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+                  alt={`Imagem de ${character.name}`}
+                />
+              </figure>
+            </div>
+            <article className="character__comics">
+              {isLoadingComics ? (
+                <div className="character__comics__loading">
+                  <span className="loading" />
+                </div>
+              ) : (
+                <>
+                  <h2>Últimos lançamentos</h2>
+                  <div className="character__comics__list">
+                    {comics?.map((comic) => (
+                      <figure key={comic.title}>
+                        <img
+                          src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
+                          alt={`Capa de ${comic.title}`}
+                        />
+                        <figcaption>{comic.title}</figcaption>
+                      </figure>
+                    ))}
+                  </div>
+                </>
+              )}
+            </article>
+            <Footer />
+          </>
+        )}
+      </main>
     </div>
   );
 };
